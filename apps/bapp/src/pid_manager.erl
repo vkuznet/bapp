9e7aa842ccbab889972bf51a219c59cbc71be2bc
Valentin Kuznetsov <vkuznet@gmail.com>
2011-09-26 18:40:56 -0400

    Switch from process_info_wrapper to pinfo

--- apps/bapp/src/pid_manager.erl
+++ apps/bapp/src/pid_manager.erl
@@ -1,8 +1,8 @@
-%% -*- mode: nitrogen -*-
+%% -*- mode: erlang -*-
 -module(pid_manager).
 -author('vkuznet@gmail.com').
 -export([start/0, info/0, add/3, info_node/1, info_pid/1, info_guid/1, delete/2]).
--export([process_info_wrapper/2]).
+-export([pinfo/2]).
 -define(TABLE, pid_table).
 
 %% ------------------------------------------------------------------
@@ -15,26 +15,38 @@ info() ->
     ets:info(?TABLE).
 
 %% ------------------------------------------------------------------
-%% Process info wrapper call to given Node, Pid.
-%% Please note: in order to make rpc:call I wrap pid to list and
-%% unwrap it back (plain pass of Pid does not work).
+%% Helper function to extract second element of Pid.
+%% For Pid structure explanation see
+%% http://stackoverflow.com/questions/243363/can-someone-explain-the-structure-of-a-pid-in-erlang
 %% ------------------------------------------------------------------
-process_info_wrapper(Node, Pid) ->
-    ProcInfo = rpc:call(Node, erlang, process_info, [list_to_pid(pid_to_list(Pid))]),
-    case ProcInfo of
-        undefined ->
-            undefined;
-        _other ->
-            [{K,V} || {K,V} <- ProcInfo, K==status]
+pid_b(Pid) ->
+    lists:nth(2, [binary_to_list(S) || S <- re:split( pid_to_list(Pid), "\\." )] ).
+
+%% ------------------------------------------------------------------
+%% Process info wrapper obtain Pid info on remote Node.
+%% We match on a second id of Pid using remote processes call, which
+%% return Pid table on remote node.
+%% ------------------------------------------------------------------
+pinfo(Node, PidStr) ->
+    List = [binary_to_list(S) || S <- re:split(PidStr, "\\.")],
+    Processes = rpc:call(Node, erlang, processes, []),
+    MatchPid = [P || P <- Processes, pid_b(P) == lists:nth(2, List) ],
+    if  length(MatchPid) == 1 ->
+        ProcInfo = rpc:call(Node, erlang, process_info, [hd(MatchPid)]),
+        case ProcInfo of
+            undefined ->
+                undefined;
+            _other ->
+                [{K,V} || {K,V} <- ProcInfo, K==status]
+        end;
+        true -> undefined
     end.
 
 %% ------------------------------------------------------------------
 %% add entry into table for any active Pid and a given node.
 %% ------------------------------------------------------------------
 add(GUID, Node, Pid) ->
-%    ProcInfo = rpc:call(Node, erlang, process_info, [list_to_pid(pid_to_list(Pid))]),
-%    case ProcInfo of
-    case process_info_wrapper(Node, Pid) of
+    case pinfo(Node, pid_to_list(Pid)) of
         undefined ->
             skip;
         _other ->
@@ -45,9 +57,7 @@ add(GUID, Node, Pid) ->
 %% delete Pid from the table based on its status
 %% ------------------------------------------------------------------
 delete(Node, Pid) ->
-%    ProcInfo = rpc:call(Node, erlang, process_info, [list_to_pid(pid_to_list(Pid))]),
-%    case ProcInfo of
-    case process_info_wrapper(Node, Pid) of
+    case pinfo(Node, pid_to_list(Pid)) of
         undefined ->
             ets:match_delete(?TABLE, {'_', '_', Pid});
         _other ->
@@ -60,18 +70,18 @@ delete(Node, Pid) ->
 %% ------------------------------------------------------------------
 info_node(Node) ->
     Tup = ets:match_object(pid_table, {'_', Node, '_'}),
-    [{G,N,P,process_info_wrapper(N,P)} || {G,N,P} <- Tup, delete(N,P) /= true].
+    [{G,N,P,pinfo(N,pid_to_list(P))} || {G,N,P} <- Tup, delete(N,P) /= true].
 
 %% ------------------------------------------------------------------
 %% provides information about given pid
 %% ------------------------------------------------------------------
 info_pid(Pid) ->
     Tup = ets:match_object(pid_table, {'_', '_', Pid}),
-    [{G,N,P,process_info_wrapper(N,P)} || {G,N,P} <- Tup].
+    [{G,N,P,pinfo(N,pid_to_list(P))} || {G,N,P} <- Tup].
 
 %% ------------------------------------------------------------------
 %% provides information about given guid
 %% ------------------------------------------------------------------
 info_guid(Guid) ->
     Tup = ets:lookup(?TABLE, Guid),
-    [{G,N,P,process_info_wrapper(N,P)} || {G,N,P} <- Tup].
+    [{G,N,P,pinfo(N,pid_to_list(P))} || {G,N,P} <- Tup].
